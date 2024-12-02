b6adca0ee4354f78eb9c0173fdfc2a49cdefbe83
Abhishek Sinha <60222964+AbhishekSinha7@users.noreply.github.com>
2024-12-02 09:45:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ menu = st.sidebar.selectbox("Menu", ["Dashboard","Analytics", "Code", "Graph","C
 file_uploader = st.file_uploader(":file_folder: Upload Your File", type=(["csv","xlsx","xls"]) )
 
 if file_uploader is not None:
-    st.write("file_uploader.type",file_uploader.type) 
-    if file_uploader.type == "text/csv":
+    #st.write("file_uploader.type",file_uploader.type) 
+    if file_uploader.type == "application/vnd.ms-excel":
         df = pd.read_csv(file_uploader)
     elif file_uploader.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
         df = pd.read_excel(file_uploader)
